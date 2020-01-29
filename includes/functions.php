59fd40fefbf19d84d985aec9bd180ba5a3e67d29
HetmesAskalana <60427521+HetmesAskalana@users.noreply.github.com>
2020-01-29 11:32:45 -0600

    Update functions.php

--- includes/functions.php
+++ includes/functions.php
@@ -1,55 +1,62 @@
 <?php
-require "mysql.php";
-$db = mysql_connect("localhost", $db_username, $db_pw);
-mysql_selcect_db("qbox", $db);
 function is_login(){
+	require "mysql.php";
+	$db = new mysqli($db_address, $db_username, $db_pw, $db_name);
 	if(empty($_COOKIE['user_token'])){
 		return false;
 	}else{
-		$cmd = "SELECT * FROM user WHERE 'user_token' = ".$_COOKIE['user_token'].";";
-		$user = mysql_query($cmd);
-		$info = mysql_fetch_array($user);
+		$cmd = "SELECT * FROM user WHERE user_token = '".$_COOKIE['user_token']."';";
+		$user = $db->query($cmd);
+		$info = $user->fetch_assoc();
 		if(empty($info['username'])) return false;
 			else return true;
 	}
 }
 function set_login($un, $pw){
-	$cmd = "SELECT * FROM user WHERE 'username' = ".$un.";";
-	$user = mysql_query($cmd);
-	$info = mysql_fetch_array($user);
+	require "mysql.php";
+	$db = new mysqli($db_address, $db_username, $db_pw, $db_name);
+	$cmd = "SELECT * FROM qbox.user WHERE username = '".$un."';";
+	$user = mysqli_query($db, $cmd);
+	$info = mysqli_fetch_array($user);
 	if(empty($info['username'])) die("用户不存在");
 	if($pw == $info['pw']){
-		setcookie("user_token", $info['token'], time()+315360000);
-		echo "<script>alert('登陆完成');parent:location.href='/index.php';</script>";
+		$time = time() + 3600;
+		setcookie("user_token", $info['user_token'], $time);
+		echo "<script>alert('登陆完成');parent:location.href='index.php';</script>";
 	}else die("密码错误");
 }
 function set_logout(){
 	setcookie("user_token", "null", time()-1);
 }
 function new_ques($name, $message){
-	$cmd = "SELECT * FROM 'user' WHERE 'username' = '".$name."';";
-	$user = mysql_query($cmd);
-	$info = mysql_fetch_array($user);
+	require "mysql.php";
+	$db = new mysqli($db_address, $db_username, $db_pw, $db_name);
+	$cmd = "SELECT * FROM user WHERE username = '".$name."';";
+	$user = $db->query($cmd);
+	$info = $user->fetch_assoc();
 	if(empty($info)) die("用户不存在");
 	$uid = $info['uid'];
-	$cmd = "INSERT INTO 'questions' ('uid', 'msg') VALUE ('".$info['uid']."', '".$msg."');";
-	mysql_query($cmd);
+	$cmd = "INSERT INTO questions (uid, msg) VALUES (".$info['uid'].", '".$message."');";
+	$db->query($cmd);
 }
 function set_readed($quesid){
+	require "mysql.php";
+	$db = new mysqli($db_address, $db_username, $db_pw, $db_name);
 	$cmd = "UPDATE questions SET 'readed' = 1 WHERE 'id' = ".$quesid.";";
-	$ques = mysql_query($cmd);
+	$ques = $db->query($cmd);
 }
 function show_ques($tok){
-	$cmd = "SELECT * FROM 'user' WHERE 'user_token' = ".$tok.";";
-	$user = mysql_query($cmd);
-	$userinfo = mysql_fetch_array($user);
-	$cmd = "SELECT * FROM questions WHERE 'id' > ".$userinfo['last_id']." and 'uid' = ".$userinfo['uid'].";";
-	$ques = mysql_query($cmd);
-	while($info = mysql_fetch_array($ques)){
-		$cmd = "UPDATE 'user' SET 'last_id' = '".$info['id']."' WHERE 'user_token' = '".$tok."';";
-		mysql_query($cmd);
-		return $info['msg'];
-		break;
-	}
+	require "mysql.php";
+	$db = new mysqli($db_address, $db_username, $db_pw, $db_name);
+	$cmd = "SELECT * FROM user WHERE user_token = '".$tok."';";
+	$user = $db->query($cmd);
+	$userinfo = $user->fetch_assoc();
+	$cmd = "SELECT * FROM questions WHERE id > ".$userinfo['last_id']." and uid = ".$userinfo['uid'].";";
+	$ques = $db->query($cmd);
+        $info = $ques->fetch_assoc();
+        if(empty($info)) echo "现在莫得问题";
+	$cmd = "UPDATE user SET last_id = ".$info['id']." WHERE user_token = '".$tok."';";
+	mysqli_query($db, $cmd);
+	echo $info['msg'];
 }
 ?>
