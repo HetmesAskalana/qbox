008d202521c93635458a11699f05988fed1e9e1f
HetmesAskalana <60427521+HetmesAskalana@users.noreply.github.com>
2020-01-30 21:17:06 +0800

    Update set_new_ques.php

--- set_new_ques.php
+++ set_new_ques.php
@@ -1,7 +1,8 @@
 <?php
 require "includes/functions.php";
+if(in_blacklist($_SERVER[REMOTE_ADDR])) die("<script>alert('正处在该用户的黑名单中');parent:location.href='new_ques.php';</script>");
 $body = $_POST['ques'];
 $username = $_POST['username'];
-if(empty($body) or empty($username)) die("不能有任何一项为空");
+if(empty($body) or empty($username)) die("<script>alert('不能有任何一项为空');parent:location.href='new_ques.php';</script>");
 new_ques($username, $body);
 ?>
