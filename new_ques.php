40dffadb8a8b08c600e8bbb6faf6c278e8c2ccfc
HetmesAskalana <60427521+HetmesAskalana@users.noreply.github.com>
2020-01-30 12:44:12 +0800

    Update new_ques.php

--- new_ques.php
+++ new_ques.php
@@ -2,5 +2,5 @@
 <form method = "post" action = "set_new_ques.php">
 <center>请输入对方的用户名：<input type = "text" name = "username" value = "<?php echo $_GET['target_un']; ?>"></center><br/>
 <center>请输入问题：<input type = "text" name = "ques"></center><br/>
-<input type = "submit" value = "下一步">
+<center><input type = "submit" value = "下一步"></center>
 </form>
