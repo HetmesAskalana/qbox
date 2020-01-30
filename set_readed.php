ab07883ca7ea80a757da16d96bea038fdbd2aebd
HetmesAskalana <60427521+HetmesAskalana@users.noreply.github.com>
2020-01-30 21:16:00 +0800

    Update set_readed.php

--- set_readed.php
+++ set_readed.php
@@ -1,4 +1,5 @@
 <?php
 require "includes/functions.php";
+if(!is_login()) die("<script>alert('请先登录');parent:location.href='login.php';</script>");
 set_readed($_COOKIE['user_token'], $_GET['qid']);
 ?>
