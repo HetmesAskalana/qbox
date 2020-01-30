f150aea4c3513f126902ff6d257e59a2c27bd681
HetmesAskalana <60427521+HetmesAskalana@users.noreply.github.com>
2020-01-30 17:34:55 +0800

    Update new_ques.php

--- new_ques.php
+++ new_ques.php
@@ -1,4 +1,5 @@
 <center><h1><font color = "blue">新建问题！</font></h1></center><br/>
+<?php error_reporting(E_ALL^E_NOTICE); ?>
 <form method = "post" action = "set_new_ques.php">
 <center>请输入对方的用户名：<input type = "text" name = "username" value = "<?php echo $_GET['target_un']; ?>"></center><br/>
 <center>请输入问题：<input type = "text" name = "ques"></center><br/>
