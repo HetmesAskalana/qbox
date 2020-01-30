9d722a9fa63ee96630d8b5896eecd47811e59938
HetmesAskalana <60427521+HetmesAskalana@users.noreply.github.com>
2020-01-30 21:15:38 +0800

    Update set_show_rule.php

--- set_show_rule.php
+++ set_show_rule.php
@@ -1,4 +1,5 @@
 <?php
 require "includes/functions.php";
+if(!is_login()) die("<script>alert('请先登录');parent:location.href='login.php';</script>");
 set_show_rule($_COOKIE['user_token'], $_POST['rule']);
 ?>
