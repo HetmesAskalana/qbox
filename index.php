9e51d6379d12c86ec6a684fe319983b65b6f6d3a
HetmesAskalana <60427521+HetmesAskalana@users.noreply.github.com>
2020-01-30 17:33:19 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,9 +2,12 @@
 <?php
 require "includes/functions.php";
 if(is_login()){
-	echo "<a href = '/user_info.php'>点击前往用户页面</a><br/>";
+	echo "<center><a href = 'user_info.php'>点击前往用户页面</a><br/></center>";
 }
 ?>
 <center><h1><font color = "blue">欢迎访问提问箱！</font></h1></center><br/>
-<center><h2><font color = "blud">新建一个问题？</font></h3></center><br/>
-<center><h3><font color = "red"><a href = "new_ques.php">点击这里！</a></font></h3></center>
+<center><h2><font color = "red"><a href = "new_ques.php">新建一个问题</a></font></h3></center><br/>
+<center><h2><font color = "red"><a href = "register.php">点击注册</a></font></h2></center>
+<center><h2><font color = "red"><a href = "login.php">点击登陆</a></font></h2></center>
+<br><br><br><br>
+<b>Copyright© Hetmes Askalana of UNACAS Org., published on GNU.</b>
