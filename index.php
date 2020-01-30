e8c5e31eeb66f61c4ac58df18984e86f4f11f1ce
HetmesAskalana <60427521+HetmesAskalana@users.noreply.github.com>
2020-01-30 19:17:31 +0800

    Update index.php
    已经登陆了显示登出不显示登陆和注册

--- index.php
+++ index.php
@@ -7,7 +7,10 @@ if(is_login()){
 ?>
 <center><h1><font color = "blue">欢迎访问提问箱！</font></h1></center><br/>
 <center><h2><font color = "red"><a href = "new_ques.php">新建一个问题</a></font></h3></center><br/>
-<center><h2><font color = "red"><a href = "register.php">点击注册</a></font></h2></center>
-<center><h2><font color = "red"><a href = "login.php">点击登陆</a></font></h2></center>
+<?php
+if(!is_login()){
+echo "<center><h2><font color = 'red'><a href = 'register.php'>点击注册</a></font></h2></center><center><h2><font color = 'red'><a href = 'login.php'>点击登陆</a></font></h2></center>";
+}else echo "<h2><a href = 'logout.php'><font color = 'blue'>登出</font></a></h2>"
+?>
 <br><br><br><br>
 <b>Copyright© Hetmes Askalana of UNACAS Org., published on GNU.</b>
