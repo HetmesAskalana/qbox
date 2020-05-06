3bf16b8af6764539540ca3f3bedb01bf92b23f31
HetmesAskalana <60427521+HetmesAskalana@users.noreply.github.com>
2020-05-06 13:56:24 +0800

    Update and rename set_register to set_register.php

--- set_register.php
+++ set_register.php
@@ -6,6 +6,6 @@ $db = new mysqli($db_address, $db_username, $db_pw, $db_name);
 $cmd = "SELECT * FROM config WHERE id = 1;";
 $cfg = $db->query($cmd);
 $info = $cfg->fetch_assoc();
-if($cfg['reg_ref_code'] != md5($_POST['rc']) and $info['reg_auth'] == 1) die("邀请码错误");
+if($info['reg_ref_code'] != md5($_POST['rc']) and $info['reg_auth'] == 1) die("邀请码错误");
 set_register($_POST['un'], $_POST['pw1'])
 ?>
